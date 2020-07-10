a4858952e5605fd4ca3084aa6f8bebe457213555
berthoud <mgb11@cornell.edu>
2020-07-10 18:05:08 -0500

    Update main to 2020 version

---
+++ webfitsviewer/src/main.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+""" Main function to be used as CGI script or to create WSGI application.
+
+    To get the correct config file: use the following in the server .conf
+        SetEnv WEBVIEW_CONFIG "/path/to/webview_config.txt"
+    To get the correct python version: set the PATH in the server .conf
+        SetEnv PATH "/opt/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
+"""
+
+# Add to pythonpath (get from config)
+import sys
+import os
+from configobj import ConfigObj # Configuration Object
+conf = ConfigObj(os.environ['WEBVIEW_CONFIG'])
+newpaths = conf['path']['pythonpath'].split('\n')
+newpaths = [p.strip() for p in newpaths]
+for np in newpaths:
+    if np not in sys.path:
+        sys.path.append(np)
+
+# Import - check if possible to import weberror.errormiddleware
+import traceback
+from wsgiref.handlers import CGIHandler
+weberror_imported = 0 # flag to indicate with error wrapper to use
+try:
+    from weberror.errormiddleware import ErrorMiddleware # @UnresolvedImport
+    weberror_imported = 1
+except:
+    pass
+
+# Define Application
+def app(environ, start_response):
+    """ Application for WSGI operation. The application creates a
+        SiteController object and calls it. The SiteController is expected
+        to call start_response. The ouput from SiteController is returned.
+    """
+    from controller import SiteController # has to be in here to catch errors
+    controller = SiteController()
+    output = controller(environ,start_response)
+    return [output]
+
+# Define Application with error handler
+#application = ErrorMiddleware(app, debug = True,
+#                              error_message = 'HAWC Viewer Error')
+
+# Define application that returns errors
+def errapp(environ, start_response):
+    """ CGI application that returns errors
+    """
+    try:
+        from controller import SiteController
+        #environ['wsgi.input']=''
+        controller = SiteController()
+        output = controller(environ,start_response)
+    except Exception, e:
+        start_response('200 OK',[('Content-Type','text/html')])
+        output = """<html><body>
+<b>Error</b> = %s<br>
+%s<p>
+<b>Path</b> = %s<p>
+<b>Diag</b> = %s
+</body>
+</html>
+"""
+        diag = "Diagnosic data:<br>\n"
+        for v in environ:
+            diag += v + " : " + repr(environ[v])+"<br>\n"
+        output = output % (repr(e), traceback.format_exc().replace('\n','<br>\n'),
+                           repr(sys.path), diag)
+    return [output]
+
+# Define WSGI application
+if weberror_imported:
+    application = ErrorMiddleware(app, debug = True,
+                                  error_message = 'HAWC Viewer Error')
+else:
+    application = errapp
+
+# Call main for the application to be callable as cgi script
+if __name__ == '__main__':
+    CGIHandler().run(application)
+    

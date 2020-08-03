6558c040c428ef8a5a76bd867ed8bf7952ffeaec
Camila C.A <cycuesta@gmail.com>
2020-08-03 15:19:30 -0500

    fixed indentation errs

--- database/user_crud/db_config.py
+++ database/user_crud/db_config.py
@@ -1,11 +1,11 @@
 from app import app
 from flaskext.mysql import MySQL
- ### change this to local mysql password
+
 mysql = MySQL()
  
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root' ### change this to your local mysql user
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root' ### change this to your local mysql password
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'astrodatabase'
 app.config['MYSQL_DATABASE_DB'] = ''
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)

de689bc26536f4d50aef8bb57861c5b11167aae8
berthoud <berthoud@10.150.134.181>
2026-05-14 11:48:48 -0500

    Bugfix

--- webfitsviewer/src/controller.py
+++ webfitsviewer/src/controller.py
@@ -80,10 +80,10 @@ class SiteController(object):
         if len(responsearr) > 0:
             siteurl = responsearr[0]
             responsearr = responsearr[1:]
+            # remove query string if present
+            if '?' in responsearr[-1]:
+                responsearr[-1] = responsearr[-1].split('?')[0]
         else: siteurl = ''
-        # remove query string if present
-        if '?' in responsearr[-1]:
-            responsearr[-1] = responsearr[-1].split('?')[0]
         # Load Configuration
         self.conf = ConfigObj(environ.get('WEBVIEW_CONFIG'))
         # Add configuration from site_sitename
